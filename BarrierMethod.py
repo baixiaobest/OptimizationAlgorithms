1a4fa3220681df728df92576a7e75197bc695a81
baixiaobest <baixiaohuang1@gmail.com>
2023-01-21 14:58:07 -0800

    Add solver for feasibility problem

--- BarrierMethod.py
+++ BarrierMethod.py
@@ -47,59 +47,70 @@ class BarrierMethod:
 
     '''
         Centering step with linear equality constraint and inequality constraint.
-        Problem is transformed into:
-            minimize: t*f(x) - sum_i(log(-fi(x)))
+        Original problem is transformed into:
+            minimize: t*f(x) - Σ_i( log(-fi(x)) )
             subject to: A*x = b
             
+        and it is solved with infeasible start newton method 
+        or newton method in there is no equality constraint.   
+        t is scaling constant
         f(x), fi(x) is R^n -> R
+        
         f: Original objective function f(x).
         f_grad: Gradient of original objective function.
         f_hess: Hessian of original objective function.
-        f_ineq: List of inequality functions.
-        f_ineq_grad: List of gradient functions of inequality functions.
-        f_ineq_hess: List of hessian matrices of inequality functions.
+        f_ineq_list: List of inequality functions.
+        f_ineq_grad_list: List of gradient functions of inequality functions.
+        f_ineq_hess_list: List of hessian matrices of inequality functions.
         x_init needs to be strictly feasible, i.e, A * x_init = b and fi(x_init) < 0.
         A, b: Equality constraint A*x = b
         term_pos_dual: Terminate the iteration when iteration enters positive dual objective.
             Dual objective is f(x) - m/t
     '''
-    def run_barrier_method(self, f, f_grad, f_hess, f_ineq, f_ineq_grad, f_ineq_hess, x_init, A, b, term_pos_dual=False):
+    def run_barrier_method(self, f, f_grad, f_hess, f_ineq_list, f_ineq_grad_list, f_ineq_hess_list,
+                           x_init, A, b, term_pos_dual=False):
 
-        m = len(f_ineq)
+        m = len(f_ineq_list)
+        x_init = x_init.astype('float64')
 
-        def get_log_barrier_grad(f_ineq, f_ineq_grad):
+        def get_log_barrier_grad(f_ineq_list, f_ineq_grad_list):
             def func(x):
-                return self._log_barrier_grad(x, f_ineq, f_ineq_grad)
+                return self._log_barrier_grad(x, f_ineq_list, f_ineq_grad_list)
             return func
 
         t = self.calculate_starting_t(
-            x_init, f_grad, get_log_barrier_grad(f_ineq, f_ineq_grad), A)
+            x_init, f_grad, get_log_barrier_grad(f_ineq_list, f_ineq_grad_list), A)
+        duality_gap = m / t
 
         x = x_init
         f_center = 0 # Objective value after each centering step.
-        while m/t > self.center_sigma and (not term_pos_dual or f_center - m / t <= 0):
+        while duality_gap > self.center_sigma and (not term_pos_dual or f_center - duality_gap <= 0):
             if A is None:
                 newton = Newton.NewtonMethod(self.alpha, self.beta, self.sigma)
                 x = newton.minimize(
-                    self._get_ff(f, f_ineq, t),
-                    self._get_ff_grad(f_grad, f_ineq, f_ineq_grad, t),
-                    self._get_ff_hess(f_hess, f_ineq, f_ineq_grad, f_ineq_hess, t),
+                    self._get_ff(f, f_ineq_list, t),
+                    self._get_ff_grad(f_grad, f_ineq_list, f_ineq_grad_list, t),
+                    self._get_ff_hess(f_hess, f_ineq_list, f_ineq_grad_list, f_ineq_hess_list, t),
                     x)
             else:
                 newton = ISNewton.InfeasibleStartNewtonMethod(self.alpha, self.beta, self.sigma)
                 x, v = newton.minimize(
-                    self._get_ff(f, f_ineq, t),
-                    self._get_ff_grad(f_grad, f_ineq, f_ineq_grad, t),
-                    self._get_ff_hess(f_hess, f_ineq, f_ineq_grad, f_ineq_hess, t),
+                    self._get_ff(f, f_ineq_list, t),
+                    self._get_ff_grad(f_grad, f_ineq_list, f_ineq_grad_list, t),
+                    self._get_ff_hess(f_hess, f_ineq_list, f_ineq_grad_list, f_ineq_hess_list, t),
                     x, A, b)
 
             f_center = f(x)
+            duality_gap = m / t
             t = 100*t
+
+        if term_pos_dual and f_center - duality_gap > 0:
+            print(f"terminate with dual objective value: {f_center - duality_gap}")
 
         # Calculate dual variable for inequalities.
         λ = np.zeros(m)
         for i in range(m):
-            fi = f_ineq[i]
+            fi = f_ineq_list[i]
             λ[i] = -1 / (t * fi(x))
 
         if A is None:
@@ -107,14 +118,108 @@ class BarrierMethod:
         else:
             return x, λ, v/t
 
-    def run_phase_1_feasibility(self):
-        pass
+    '''
+    This function solve the feasibility problem, systems of inequalities and equalities :
+        fi(x) <= 0 and A@x = b
+        
+    The above problem is formulated as:
+        minimize s
+        subject to: fi(x) <= s
+                    A@x = b
+    If the solution of the above problem has primal objective less than equal to 0, i.e. s <= 0,
+    then we have a solution to the systems of inequalities and equalities.
+    
+    The above problem again is transformed into objective with log barrier:
+        minimize s - Σ_i( log(s - fi(x)) )
+        subject to: A@x = b
+    and solved with infeasible start newton method.
+    Variables are x in R_n and s in R.
+    '''
+    def solve_feasibility(self, f_ineq_list, f_ineq_grad_list, f_ineq_hess_list, x_init, A, b):
+        m = len(f_ineq_list)
+        n = x_init.shape[0]
+        x_init = x_init.astype('float64')
 
-    def _get_ff(self, f, f_ineq, t):
+        # Here, var is [x, s], x in R_n, s in R
+        def f_mod(var):
+            return var[-1]
+
+        def f_grad_mod(var):
+            l = var.shape[0]
+            grad = np.zeros(l)
+            grad[-1] = 1
+            return grad
+
+        def f_hess_mod(var):
+            l = var.shape[0]
+            hess = np.zeros((l, l))
+            return hess
+
+        # fi is R^n -> R, inequality function.
+        # modified inequality fi is fi_mod(x, s) = fi(x) - s
+        def get_fi_ineq_mod(fi):
+            def fi_ineq_mod(var):
+                x = var[0:-1]
+                s = var[-1]
+                return fi(x) - s
+
+            return fi_ineq_mod
+
+        # fi_grad is R^n -> R^n, gradient function.
+        # Modified gradient of fi is ∇fi_mod(x, s) = [∇fi(x), -1]
+        def get_fi_ineq_grad_mod(fi_grad):
+            def fi_ineq_grad_mod(var):
+                x = var[0:-1]
+                grad = np.concatenate((fi_grad(x), np.array([-1])))
+                return grad
+            return fi_ineq_grad_mod
+
+        # Modified hessian of inequality fi is ∇^2 fi_mod(x, s) = [[∇^2fi(x), 0],
+        #                                                          [0,         0]]
+        def get_fi_ineq_hess_mod(fi_hess):
+            def fi_ineq_hess_mod(var):
+                l = var.shape[0]
+                n = l-1
+                x = var[0:-1]
+                hess = np.zeros((l, l))
+                hess[0:n, 0:n] = fi_hess(x)
+
+                return hess
+            return fi_ineq_hess_mod
+
+        f_ineq_mods = []
+        f_ineq_grad_mods = []
+        f_ineq_hess_mods = []
+        s_init = 0.0
+        for i in range(m):
+            fi = f_ineq_list[i]
+            fi_grad = f_ineq_grad_list[i]
+            fi_hess = f_ineq_hess_list[i]
+            f_ineq_mods.append(get_fi_ineq_mod(fi))
+            f_ineq_grad_mods.append(get_fi_ineq_grad_mod(fi_grad))
+            f_ineq_hess_mods.append(get_fi_ineq_hess_mod(fi_hess))
+
+            s_init = max(s_init, fi(x_init))
+
+        s_init = s_init + 1
+        var_init = np.concatenate((x_init, np.array([s_init])))
+        A_aug = None
+        if not A is None:
+            A_aug = np.concatenate((A, np.zeros((m, 1))), axis=1)
+
+        var, λ, v = \
+            self.run_barrier_method(
+                f_mod, f_grad_mod, f_hess_mod,
+                f_ineq_mods, f_ineq_grad_mods, f_ineq_hess_mods,
+                var_init, A_aug, b, term_pos_dual=True)
+
+        return var[0:n], var[-1], λ, v
+
+    def _get_ff(self, f, f_ineq_list, t):
         # Function t*f(x) - sum_i(log(-fi(x)))
         def ff(x):
             log_barrier = 0
-            for fi in f_ineq:
+            for fi in f_ineq_list:
                 neg_fi = -fi(x)
                 if neg_fi > 0:
                     log_barrier += -np.log(neg_fi)
@@ -127,42 +232,42 @@ class BarrierMethod:
         return ff
 
     # gradient of log barrier, d/dx (- sum_i(log(-fi(x))))
-    def _log_barrier_grad(self, x, f_ineq, f_ineq_grad):
+    def _log_barrier_grad(self, x, f_ineq_list, f_ineq_grad_list):
         n = x.shape[0]
-        m = len(f_ineq)
+        m = len(f_ineq_list)
         lb_grad = np.zeros(n)
         for i in range(m):
-            fi = f_ineq[i]
+            fi = f_ineq_list[i]
             # x is out of domain of -log(-fi(x)).
             if fi(x) >= 0:
                 # Signal the infeasible start newton method
                 # that the dual residual is infinite.
                 return np.ones(n) * np.inf
-            fi_grad = f_ineq_grad[i]
+            fi_grad = f_ineq_grad_list[i]
             lb_grad += -1 / fi(x) * fi_grad(x)
 
         return lb_grad
 
-    def _get_ff_grad(self, f_grad, f_ineq, f_ineq_grad, t):
+    def _get_ff_grad(self, f_grad, f_ineq_list, f_ineq_grad_list, t):
         # Gradient of objective: d/dx (t*f(x) - sum_i(log(-fi(x))))
         def ff_grad(x):
-            lb_grad = self._log_barrier_grad(x, f_ineq, f_ineq_grad)
+            lb_grad = self._log_barrier_grad(x, f_ineq_list, f_ineq_grad_list)
 
             return t * f_grad(x) + lb_grad
 
         return ff_grad
 
-    def _get_ff_hess(self, f_hess, f_ineq, f_ineq_grad, f_ineq_hess, t):
+    def _get_ff_hess(self, f_hess, f_ineq_list, f_ineq_grad_list, f_ineq_hess_list, t):
         # Hessian of objective: d^2/dx^2 (t*f(x) - sum_i(log(-fi(x))))
         def ff_hess(x):
             n = x.shape[0]
-            m = len(f_ineq)
+            m = len(f_ineq_list)
             log_barrier_hess = np.zeros((n, n))
             for i in range(m):
-                fi = f_ineq[i]
-                fi_grad = f_ineq_grad[i]
+                fi = f_ineq_list[i]
+                fi_grad = f_ineq_grad_list[i]
                 fi_grad_mat = np.array([fi_grad(x)])
-                fi_hess = f_ineq_hess[i]
+                fi_hess = f_ineq_hess_list[i]
                 log_barrier_hess += 1 / (fi(x) ** 2) * fi_grad_mat.T @ fi_grad_mat - 1 / fi(x) * fi_hess(x)
 
             return t * f_hess(x) + log_barrier_hess
--- tests/TestBarrierMethod.py
+++ tests/TestBarrierMethod.py
@@ -112,6 +112,84 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(np.allclose(x2, np.array([18 / 5, 2 / 5, 0]), atol=1e-3))
         self.assertTrue(np.allclose(x3, np.array([0, 4, 0]), atol=1e-3))
 
+    def test_feasibility(self):
+        fi_1 = self.create_affine_inequality(np.array([2 / 3, -1]), 2)
+        fi_2 = self.create_affine_inequality(np.array([1, 1]), 4)
+        fi_3 = self.create_affine_inequality(np.array([-1, 0]), 0)
+        fi_ineq = [fi_1, fi_2, fi_3]
+        fi_ineq_grad = []
+        fi_ineq_hess = []
+        for fi in fi_ineq:
+            fi_ineq_grad.append(get_auto_gradient(fi))
+            fi_ineq_hess.append(get_auto_hessian(fi))
+
+        bm = BM.BarrierMethod(alpha=0.5, beta=0.1, sigma=1e-5, center_sigma=1e-5)
+
+        x_init = np.array([100, 100])
+        x, s, _, _ = bm.solve_feasibility(fi_ineq, fi_ineq_grad, fi_ineq_hess, x_init, None, None)
+
+        self.assertTrue(s <= 0)
+        for fi in fi_ineq:
+            self.assertTrue(fi(x) <= 0)
+
+    def test_feasibility_2(self):
+        np.random.seed(4)
+        f_ineq = []
+        x_sol = np.array([10, 21, 32, -12, 34, 23, 53, 12, 42, 100])
+        tolerance = 1
+        n = 10
+        N = 100
+        for i in range(N):
+            a_rand = np.random.rand(n) * 10
+            b = a_rand@x_sol + tolerance
+            f_ineq.append(self.create_affine_inequality(a_rand, b))
+
+        fi_ineq_grad = []
+        fi_ineq_hess = []
+        s_max_init = 0
+        x_init = np.ones(n) * 1000
+        for fi in f_ineq:
+            fi_ineq_grad.append(get_auto_gradient(fi))
+            fi_ineq_hess.append(get_auto_hessian(fi))
+            s_max_init = max(s_max_init, fi(x_init))
+
+        bm = BM.BarrierMethod(alpha=0.5, beta=0.1, sigma=1e-5, center_sigma=1e-5)
+
+        x, s, _, _ = bm.solve_feasibility(f_ineq, fi_ineq_grad, fi_ineq_hess, x_init, None, None)
+
+        s_max_sol = 0
+        for fi in f_ineq:
+            self.assertTrue(fi(x) <= 0)
+            s_max_sol = max(s_max_sol, fi(x))
+
+        print(f"s_max_init: {s_max_init} \ns_max_sol: {s_max_sol}")
+
+    def test_infeasibility(self):
+        fi_1 = self.create_affine_inequality(np.array([2 / 3, -1]), -2)
+        fi_2 = self.create_affine_inequality(np.array([1, 1]), -4)
+        fi_3 = self.create_affine_inequality(np.array([-1, 0]), 0)
+        fi_ineq = [fi_1, fi_2, fi_3]
+        fi_ineq_grad = []
+        fi_ineq_hess = []
+        for fi in fi_ineq:
+            fi_ineq_grad.append(get_auto_gradient(fi))
+            fi_ineq_hess.append(get_auto_hessian(fi))
+
+        bm = BM.BarrierMethod(alpha=0.5, beta=0.1, sigma=1e-5, center_sigma=1e-5)
+
+        x_init = np.array([100, 100])
+        x, s, _, _ = bm.solve_feasibility(fi_ineq, fi_ineq_grad, fi_ineq_hess, x_init, None, None)
+
+        feasible = True
+        s_max_sol = 0
+        for fi in fi_ineq:
+            if(fi(x) > 0):
+                feasible = False
+            s_max_sol = max(s_max_sol, fi(x))
+
+        self.assertFalse(feasible)
+        print(f"s_max_sol: {s_max_sol}")
+
 
 if __name__ == '__main__':
     unittest.main()
